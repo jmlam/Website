e94f23d64b71727182342419cb3e8cd3cb45510d
Jeremiah Lam <jiamialam@gmail.com>
2020-02-11 10:17:11 -0800

    added html templates through render_template

--- script.py
+++ script.py
@@ -1,4 +1,4 @@
-from flask import Flask #importing flask class object
+from flask import Flask, render_template #importing flask class object, render template accesses store html files and displays the html
 
 app=Flask(__name__) #instantiating flask object. __name__ gets value of the name of the python script
 #when a script is run the script is given the name __main__
@@ -7,11 +7,11 @@ app=Flask(__name__) #instantiating flask object. __name__ gets value of the name
 
 @app.route('/') #this signifies stage in the path. currently this is the home page, or just the beginning. 
 def home():
-    return 'Homepage goes here!'
+    return render_template('home.html')
 
 @app.route('/Portfolio/')
 def portfolio():
-    return 'Look at my work!'
+    return render_template('portfolio.html')
 
 
 if __name__ == "__main__":  #if we are executing from the main script then the app will run. but if it is imported then the app will not run
